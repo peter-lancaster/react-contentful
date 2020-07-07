4601df398737decb5a3b2a44f188d92064052510
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-07-07 10:59:35 +0100

    first commit after local copy

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import React, {useContext} from 'react';
 import './App.css';
 import {Context} from "./ContextProvider"
 import {Link} from "react-router-dom"
+import BlogItem from "./components/BlogItem"
 
 
 function App() {
 
   console.log("App")
 
-  const {posts} = useContext(Context)
+  const {allPosts} = useContext(Context)
 
-  console.log("this is posts in App")
-  console.log(posts)
-  // console.log("this is changeVariable2 in App")
-  // console.log(changeVariable2)
+  // console.log("this is posts in App")
+  // console.log(posts)
+
+  const blogPostList = allPosts.map(function(element, index) {
+
+    console.log("in map")
+    console.log(element.fields.description.content.value)
+    return (<BlogItem key={index} itemDetails={element.fields}   />)
+  })
+
+
+  console.log("this is blogPostList in App")
+
+  console.log(blogPostList)
+
 
 
   return (
     <div className="App">
-      <p>Some text in there</p>
+      {blogPostList}
 
     </div>
   );
